871c120256a6f28ebcc85cbb6ae28de12eacd4f6
csokaandras <csokaandras@turr.hu>
2024-12-01 22:44:34 +0100

    Idő beállítása serial monitorról

--- src/main.cpp
+++ src/main.cpp
@@ -11,12 +11,17 @@
 #define dirB 13
 #define setTimePin A2
 
+int incomingByte = 0; // for incoming serial data
+String typed = "";
+String prevTyped = "";
+
+
 // --------------------------------- IMPORTANT ---------------------------------
 bool SET_TIME_START = true;
 // --------------------------------- IMPORTANT ---------------------------------
 
 const byte powerLossPin = 2;
-const byte interruptPin = 3;
+const byte onMinPin = 3;
 RTCTime currentTime;
 const int eeAdress = 1;
 
@@ -27,8 +32,10 @@ int diffinmin = 0;
 int currentmin = 0;
 int currentsec = 0; // only for monitoring
 int rotationCounter = 0;
+
 bool schangeShow = true;
 bool allOkay = true;
+bool monitorTime = false;
 
 struct DateTime{
   int year;
@@ -39,6 +46,8 @@ struct DateTime{
   int min;
   int sec;
 };
+
+RTCTime newSaveDateTime;
 DateTime savedDateTime;
 DateTime showedDateTime;
 
@@ -134,6 +143,28 @@ void calculateDifference(DateTime current, DateTime saved) {
   calculateMinDiff(current.min, saved.min);
 }
 
+void showTimeDatas(){
+  Serial.println();
+  printDate(convert2RTC(savedDateTime),   "Az mentett idő:  ");
+  printDate(convert2RTC(showedDateTime),  "Az aktuális idő: ");
+  printDate(currentTime,                  "Az pontos idő:   ");
+}
+
+void PrintVT100(){
+  Serial.println();
+  Serial.println("          ▞                                                                                                                   ▞                ");
+  Serial.println("┏━━━┓  ┏━━━━┓ ━━┳━━ ┏┓  ┏┓ ┏━━━━┓ ┏┓   ┰ ┏━━━━┓ ┏━━━━┓ ━━┳━━ ┏━━━━┓ ┏━━━━┓        ━━┳━━ ┏━━━━┓ ┏━━━━┓ ┏━━━━┓ ┏┓   ┰  ┓   ┏ ┏━━━━┓ ┏━━━━┓ ┏━━━━┓");
+  Serial.println("┃   ┃  ┃    ┃   ┃   ┃┗┓┏┛┃ ┃    ┃ ┃┗┓  ┃ ┃    ┃ ┗┓       ┃   ┃    ┃ ┃    ┃          ┃   ┃    ┃ ┃    ┃ ┃    ┃ ┃┗┓  ┃  ┗┓ ┏┛ ┃    ┃ ┃    ┃ ┃    ┃");
+  Serial.println("┣━━━┻┓ ┣━━━━┫   ┃   ┃ ┗┛ ┃ ┃    ┃ ┃ ┗┓ ┃ ┃    ┃  ┗━━┓    ┃   ┃    ┃ ┣━━━┳┛          ┃   ┃    ┃ ┣━━━┳┛ ┃    ┃ ┃ ┗┓ ┃   ┗┳┛  ┃    ┃ ┣━━━┳┛ ┣━━━━┫");
+  Serial.println("┃    ┃ ┃    ┃   ┃   ┃    ┃ ┃    ┃ ┃  ┗┓┃ ┃    ┃     ┗┓   ┃   ┃    ┃ ┃   ┗┓          ┃   ┃    ┃ ┃   ┗┓ ┃    ┃ ┃  ┗┓┃    ┃   ┃    ┃ ┃   ┗┓ ┃    ┃");
+  Serial.println("┗━━━━┛ ┚    ┖   ┸   ┸    ┸ ┗━━━━┛ ┸   ┗┛ ┗━━━━┛ ┗━━━━┛   ┸   ┗━━━━┛ ┸    ┸          ┸   ┗━━━━┛ ┸    ┸ ┗━━━━┛ ┸   ┗┛    ┸   ┗━━━━┛ ┸    ┸ ┚    ┖");
+  Serial.println("☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲☴☱☲");
+  Serial.println("                                                              Developed By: Csóka András");
+  Serial.println("                                                               Designed By: Csóka Antal");
+  Serial.println();
+  showTimeDatas();
+}
+
 void stopRotate() {
 
   if (diffinmin > 0) {
@@ -172,9 +203,10 @@ void setup() {
   pinMode(powerLossPin, INPUT_PULLUP);
   attachInterrupt(digitalPinToInterrupt(powerLossPin), saveTime, RISING);
   
-  pinMode(interruptPin, INPUT_PULLUP);
-  attachInterrupt(digitalPinToInterrupt(interruptPin), stopRotate, RISING);
+  pinMode(onMinPin, INPUT_PULLUP);
+  attachInterrupt(digitalPinToInterrupt(onMinPin), stopRotate, RISING);
 
+  // az elmentett idő és az óra szerinti közt kiszámolja a különbséget
   EEPROM.get(eeAdress, savedDateTime);
   calculateDifference(convert2DT(currentTime), savedDateTime);
   showedDateTime = savedDateTime;
@@ -182,7 +214,7 @@ void setup() {
   myStepper.setSpeed(60);
 
   if (!RTC.isRunning()) {
-    // this means the RTC is waking up "as new"
+    // ha az óra eleme lemerült
     if (savedTime.getYear() == 2000) {
       allOkay = false;
     } else {
@@ -223,16 +255,111 @@ void loop() {
     }
 
     // monitoring
-    if (currentsec != currentTime.getSeconds())
+
+    if (currentsec != currentTime.getSeconds() && monitorTime)
     {
       currentsec = currentTime.getSeconds();
-      printDate(convert2RTC(savedDateTime), "Saved time: ");
-      printDate(convert2RTC(showedDateTime), "Showed time: ");
-      printDate(currentTime, "Current time: ");
+
+      showTimeDatas();
 
       Serial.print(diffinmin);
       Serial.println();
     }
+
+    if (Serial.available() > 0) {
+    // read the incoming byte:
+      incomingByte = Serial.read();
+      //Serial.print(incomingByte);
+      switch (incomingByte)
+      {
+        case 10:
+          typed = "";
+          prevTyped = "";
+          Serial.println();
+          break;
+
+        case 27:
+          typed = "";
+          Serial.println("Idő folyamatos kijelzése befejezve");
+          Serial.println();
+          monitorTime = false;
+          break;
+
+        case 13:
+          if (prevTyped == "-b")
+          {
+            DateTime newTime;
+            newTime.year = typed.substring(0,4).toInt();
+            newTime.month = typed.substring(5,7).toInt();
+            newTime.day = typed.substring(8,10).toInt();
+            newTime.hour = typed.substring(11,13).toInt();
+            newTime.min = typed.substring(14,16).toInt();
+            newTime.sec = typed.substring(17,19).toInt();
+            Serial.println();
+            printDate(convert2RTC(newTime), "Új dátum: ");
+            RTCTime newRtc = convert2RTC(newTime);
+            RTC.setTime(newRtc);
+          }
+          break;
+          // 2024/12/01-22:26:00
+
+        default:
+          Serial.print(char(incomingByte));
+          typed += char(incomingByte);
+          break;
+      }      
+
+      if (typed == "VT100")
+      {
+        PrintVT100();
+        typed = "";
+      }
+
+      if (typed == "-cat")
+      {
+        Serial.println("\n");
+        Serial.println(" ╱|、");
+        Serial.println("(˚ˎ。7");
+        Serial.println("|、˜〵");    
+        Serial.println("じしˍ,)ノ");
+        Serial.println();
+        typed = "";
+      }
+
+      if (typed == "-h")
+      {
+        Serial.println("\n");
+        Serial.println("VT100   kezdőlap megjelenítése");
+        Serial.println("-h      segítség kiírása");
+        Serial.println("-m      idő folyamatos megjelenítésének bekapcsolása, megállításahoz nyomja meg az ESC gombot");
+        Serial.println("-b      aktuális idő beállítása");
+        Serial.println();
+        Serial.println("FONTOS");
+        Serial.println("Ha törölni kell egy sorban akkor nyomjon ENTERT-t és kezdje előlről, nem tud karaktert törölni!");
+        Serial.println();
+        typed = "";
+      }
+
+      if (typed == "-m")
+      {
+        Serial.println();
+        Serial.println("Megállításhoz nyomja meg az ESC gombot");
+        Serial.println();
+        monitorTime = true;
+        typed = "";
+      }
+
+      if (typed == "-b")
+      {
+        Serial.println();
+        Serial.println("Aktuális idő beállítása");
+        Serial.println("Elfogadáshoz nyomja meg a TAB gombot");
+        Serial.println("Kérem ebben a formátumban adja meg ÉÉÉÉ/HH/NN-óó:pp:mm");
+        prevTyped = typed;
+        
+
+        typed = "";
+      }
+    }
   }
-  
 }
